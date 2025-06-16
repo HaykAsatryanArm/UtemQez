4d653c961621c6ac429aa4d2b975ab1ea396a0c7
Hayk Asatryan <hayk.asatryan.arm@gmail.com>
2025-06-16 13:13:22 +0400

    Update

--- app/src/main/java/com/haykasatryan/utemqez/BuildConfigMy.java
+++ app/src/main/java/com/haykasatryan/utemqez/BuildConfigMy.java
@@ -1,5 +1,5 @@
 package com.haykasatryan.utemqez;
 
 public class BuildConfigMy {
-    public static String apiKey = "AIzaSyAT-a4ZQTsJwjfBQ3bsw1vwofzcxzHJ9BY";
+    public static String apiKey = "***";
 }
